7fdfc6a43231f59ce30fae9a3e682477bea05181
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2024-10-10 19:29:15 +0800

    change route

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
             <Route path="/" element={<OutsideLayout />}>
               <Route path="/login" element={<Login />} />
             </Route>
-            <Route path="/" element={<FullLayout />}>
+            <Route element={<FullLayout />}>
               <Route path="/user" element={<BpmnDiagram />} />
             </Route>
           </Routes>
